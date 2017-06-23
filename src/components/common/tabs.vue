df38606a1c1246361f3171dbe052f3908275b933
wangzailong <1715587588@qq.com>
2017-06-23 18:15:15 +0800

    tabs

---
+++ src/components/common/tabs.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="tabs" v-if="flag">
+        <div class="tabs-nav">
+            <ul>
+                <li class="tabs-nav-item" v-for="(tab,index) in tabsParam" :class="{active:tab.showFlag >= 0 }" @click="toggleTags(index)"><a href="#">{{tab.key}}</a></li>
+            </ul>
+        </div>
+        
+        <div class="tabs-content">
+           <slot>2</slot> 
+       </div>
+    </div>
+</template>
+<script>
+    export default {
+        props:{
+            flag:{
+                type:String,
+                required:true,
+                twoWay:true    
+            },
+            tabsParam:{
+                type:Array,
+                required:true
+            },
+            
+        },
+        methods:{
+            toggleTags:function (index) {
+               let _this = this
+               _this.tabsParam.forEach(function(item){
+                   item.showFlag = -1;
+               })
+               _this.tabsParam[index].showFlag = 1;
+            }
+        },
+        data(){
+            return {
+                
+            }
+        }
+    }
+</script>
+<style lang="scss" scoped>
+ @import '../../style/mixin';
+    .active a{
+        color:red;
+    }
+    ul{
+        white-space:nowrap;
+        @include wh(100%,1.6rem);
+        background:#f5f5f5;
+        @include cl;
+        overflow-x:auto;
+    }
+   .tabs-nav-item{
+        display:inline-block;
+        @include wh(2.4rem,100%);
+        text-align:center;
+        a{
+            font-size:0.8rem;
+        }
+    }
+</style>
--- src/page/home/home.vue
+++ src/page/home/home.vue
@@ -3,6 +3,25 @@
         <head-top :head-title="headTitle" main="true">
              
         </head-top>
+        <section id="main">
+            <div>
+                <tabs flag='true' :tabsParam='tabsParam'></tabs>
+                <div class="tabs-body">
+                    <div v-if="tabsParam[0].showFlag > 0" >
+                        111
+                    </div>
+                    <div v-if="tabsParam[1].showFlag > 0">
+                        222
+                    </div>
+                    <div v-if="tabsParam[2].showFlag > 0">
+                        33
+                    </div>
+                    <div v-if="tabsParam[3].showFlag > 0">
+                        111
+                    </div>
+                </div>
+            </div>
+        </section>
         <foot-bottom></foot-bottom>
     </div>
 </template>
@@ -10,12 +29,51 @@
 <script>
 import headTop from '../../components/header/head';
 import footBottom from '../../components/footer/footer';
-
+import tabs from '../../components/common/tabs'
     export default {
     data(){
         return {
             message:'sdaf',
             headTitle: '',
+            tabsParam:[
+                {
+                    key:'推荐',
+                    showFlag:1
+                },{
+                    key:'热点',
+                    showFlag:-1
+                },{
+                    key:'视频',
+                    showFlag:-1
+                },{
+                    key:'娱乐',
+                    showFlag:-1
+                },{
+                    key:'体育',
+                    showFlag:-1
+                },{
+                    key:'上海',
+                    showFlag:-1
+                },{
+                    key:'搞笑',
+                    showFlag:-1
+                },{
+                    key:'财经',
+                    showFlag:-1
+                },{
+                    key:'科技',
+                    showFlag:-1
+                },{
+                    key:'汽车',
+                    showFlag:-1
+                },{
+                    key:'美图',
+                    showFlag:-1
+                },{
+                    key:'段子',
+                    showFlag:-1
+                },
+            ]
         }
     },
     created(){
@@ -23,7 +81,8 @@ import footBottom from '../../components/footer/footer';
     },
     components:{
         headTop,
-        footBottom
+        footBottom,
+        tabs
     },
     methods:{
         initData(){
@@ -33,6 +92,8 @@ import footBottom from '../../components/footer/footer';
 }
 </script>
 <style lang="scss" scoped>
-    
+    .tabs-body{
+        background:#fff;
+    }
     
 </style>
